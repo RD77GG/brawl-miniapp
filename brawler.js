52ab0c7a9cc92e06341d3b9765dbd989007aa96a
RD77GG <v.irushin@icloud.com>
2026-08-03 00:43:55 +0500

    Update brawler.js

--- brawler.js
+++ brawler.js
@@ -1,1 +1,33 @@
+const params = new URLSearchParams(window.location.search);
+const name = params.get("name");
 
+fetch("data/brawlers.json")
+.then(response => response.json())
+.then(brawlers => {
+
+const brawler = brawlers.find(b => b.name === name);
+
+document.getElementById("name").innerHTML = brawler.name;
+
+document.getElementById("brawler").innerHTML = `
+
+<img src="${brawler.image}" class="brawler-image">
+
+<h2>Скины:</h2>
+
+<div class="skins">
+
+${brawler.skins.map(skin => `
+
+<div class="skin-card">
+<img src="${skin.image}">
+<h3>${skin.name}</h3>
+</div>
+
+`).join("")}
+
+</div>
+
+`;
+
+});
