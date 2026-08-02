3b5bf5bdaf6bf3a476d9adf6c511716b8b8569e9
RD77GG <v.irushin@icloud.com>
2026-08-03 02:29:52 +0500

    Update brawler.js

--- brawler.js
+++ brawler.js
@@ -1,6 +1,7 @@
 const params = new URLSearchParams(window.location.search);
 const name = params.get("name");
 
+
 fetch("data/brawlers.json")
 .then(response => response.json())
 .then(brawlers => {
@@ -8,23 +9,68 @@ fetch("data/brawlers.json")
 const brawler = brawlers.find(b => b.name === name);
 
 
+if(!brawler){
+    document.getElementById("brawler").innerHTML = "Боец не найден";
+    return;
+}
+
+
 document.getElementById("name").innerHTML = brawler.name;
 
 
 document.getElementById("brawler").innerHTML = `
 
+
 <div class="brawler-main">
 
+
 <img src="${brawler.image}" class="main-image">
+
+
+<div class="info">
+
+<p>⭐ Редкость: ${brawler.rarity}</p>
+
+<p>⚔️ Класс: ${brawler.class}</p>
+
+</div>
+
+
+<h2>
+Титулы
+</h2>
+
+
+<div class="titles">
+
+${brawler.titles.map(title => `
+
+<div class="title-card ${title.style}">
+
+<span>${title.name}</span>
+
+<small>${title.prime} Прайм</small>
+
+</div>
+
+`).join("")}
+
+
+</div>
+
+
 
 <h2>
 Скины (${brawler.skins.length})
 </h2>
 
+
 </div>
 
 
+
 <div class="skins">
+
 
 ${brawler.skins.map(skin => `
 
@@ -38,7 +84,9 @@ ${brawler.skins.map(skin => `
 
 `).join("")}
 
+
 </div>
+
 
 `;
 
