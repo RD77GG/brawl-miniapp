108ce2f7d6d73354ad23bf5ec25f73fb0e166c2e
RD77GG <v.irushin@icloud.com>
2026-08-03 02:37:07 +0500

    Update brawler.js

--- brawler.js
+++ brawler.js
@@ -1,93 +1,55 @@
 const params = new URLSearchParams(window.location.search);
 const name = params.get("name");
 
-
 fetch("data/brawlers.json")
 .then(response => response.json())
 .then(brawlers => {
 
-const brawler = brawlers.find(b => b.name === name);
+    const brawler = brawlers.find(b => b.name === name);
 
+    if (!brawler) {
+        document.getElementById("brawler").innerHTML = "<h2>Боец не найден</h2>";
+        return;
+    }
 
-if(!brawler){
-    document.getElementById("brawler").innerHTML = "Боец не найден";
-    return;
-}
+    document.getElementById("name").textContent = brawler.displayName;
 
+    document.getElementById("brawler").innerHTML = `
 
-document.getElementById("name").innerHTML = brawler.displayName;
+    <div class="brawler-main">
 
+        <img src="${brawler.image}" class="main-image">
 
-document.getElementById("brawler").innerHTML = `
+        <div class="info">
+            <p>⭐ <b>Редкость:</b> ${brawler.rarity}</p>
+            <p>⚔️ <b>Класс:</b> ${brawler.class}</p>
+        </div>
 
+        <h2>Титулы</h2>
 
-<div class="brawler-main">
+        <div class="titles">
+            ${brawler.titles.map(title => `
+                <div class="title-card ${title.style}">
+                    <span>${title.name}</span>
+                    <small>${title.prime} Прайм</small>
+                </div>
+            `).join("")}
+        </div>
 
+        <h2>Скины (${brawler.skins.length})</h2>
 
-<img src="${brawler.image}" class="main-image">
+    </div>
 
+    <div class="skins">
 
-<div class="info">
+        ${brawler.skins.map(skin => `
+            <div class="skin-card">
+                <img src="${skin.image}" alt="${skin.displayName}">
+                <h3>${skin.displayName}</h3>
+            </div>
+        `).join("")}
 
-<p>⭐ Редкость: ${brawler.rarity}</p>
+    </div>
 
-<p>⚔️ Класс: ${brawler.class}</p>
-
-</div>
-
-
-<h2>
-Титулы
-</h2>
-
-
-<div class="titles">
-
-${brawler.titles.map(title => `
-
-<div class="title-card ${title.style}">
-
-<span>${title.name}</span>
-
-<small>${title.prime} Прайм</small>
-
-</div>
-
-`).join("")}
-
-
-</div>
-
-
-
-<h2>
-Скины (${brawler.skins.length})
-</h2>
-
-
-</div>
-
-
-
-<div class="skins">
-
-
-${brawler.skins.map(skin => `
-
-<div class="skin-card">
-
-<img src="${skin.image}">
-
-<h3>${skin.name}</h3>
-
-</div>
-
-`).join("")}
-
-
-</div>
-
-
-`;
-
+    `;
 });
