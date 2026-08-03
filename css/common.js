f72ec8e57e82373ad99630732a89049a0de65948
RD77GG <v.irushin@icloud.com>
2026-08-03 20:06:17 +0500

    Update common.js

--- css/common.js
+++ css/common.js
@@ -1,1 +1,52 @@
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    font-family:Arial,sans-serif;
+}
 
+body{
+    background:#1b1b2f;
+    color:white;
+    min-height:100vh;
+}
+
+
+h1{
+    font-size:32px;
+}
+
+
+.back{
+    background:#6b6b78;
+    color:white;
+    border:none;
+    padding:10px 18px;
+    border-radius:12px;
+    font-size:18px;
+    font-weight:bold;
+    cursor:pointer;
+
+    position:fixed;
+    left:20px;
+    top:20px;
+
+    z-index:100;
+    transition:.2s;
+}
+
+.back:hover{
+    background:#858593;
+    transform:scale(1.05);
+}
+
+
+/* Общие карточки */
+
+.card,
+.skin-card,
+.info-card,
+.titles-card{
+    background:#2b2b45;
+    border-radius:18px;
+}
